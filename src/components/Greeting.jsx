01986c010ca2de7c3dc2da9b87a361570d34ed4d
VishnuSathish <vishnusathishv@gmail.com>
2026-01-08 11:31:19 +0530

    Greeting test

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+const Greeting = () => {
+  return (
+    <h1 data-testid="greeting">
+      Hello, welcome to the Github Actions Project 🚀
+    </h1>
+  );
+};
+
+export default Greeting;
---
+++ src/components/Greeting.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+describe("Greeting component", () => {
+  it("renders greeting text", () => {
+    render(<Greeting />);
+
+    const greetingElement = screen.getByTestId("greeting");
+    expect(greetingElement).toBeInTheDocument();
+    expect(greetingElement).toHaveTextContent(
+      "Hello, welcome to the Github Actions Project"
+    );
+  });
+});
